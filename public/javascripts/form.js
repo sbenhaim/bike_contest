3e90f4d44ac5b2e5df9c79c9cbd9e853cee63a3c
Selah <selah@clockwork.net>
2008-05-01 15:56:42 -0500

    working on form validation. forever.

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -4,31 +4,31 @@ var Validator = {
 		this.invalid = [];
 		this.submit = this.form.find(":button");
 		this.submit.attr('disabled', true);
-		this.fields = this.form.find(":input:not(:button)");
+		this.fields = this.form.find(":input:not(:button):not(:hidden)");
 		this.fields.blur( this.validate );
 		this.fields.keypress( this.validate );
 		
 		$.each(this.fields, function(i, field) {
-			this.validate({target: field});
+			Validator.validate({target: field});
 		});
 	},
 	validate: function(e) {
 		if ($(e.target).hasClass('required')) {
-			test_required($(e.target));
+			Validator.test_required($(e.target));
 		}
 		if ($(e.target).hasClass('future')) {
-			test_future($(e.target));
+			Validator.test_future($(e.target));
 		}
 	},
 	test_required: function( el ) {
-		if (!el.val().match(/^\s*$/)) pass(el);
-		else fail(el);
+		if (!el.val().match(/^\s*$/)) this.pass(el);
+		else this.fail(el);
 	},
 	pass: function( el ) {
-		if (invalid.indexOf(el) > -1) this.invalid.remove(el);
-		if (invalid.length == 0) submit.attr('disabled', false);
+		if (this.invalid.indexOf(el) > -1) this.invalid.remove(el);
+		if (this.invalid.length == 0) this.submit.attr('disabled', false);
 	},
 	fail: function ( el ) {
-		if (invalid.indexOf(el) > -1) this.invalid.push(el);
+		if (this.invalid.indexOf(el) > -1) this.invalid.push(el);
 	}
 }
